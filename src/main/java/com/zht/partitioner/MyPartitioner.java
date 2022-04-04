3bbb4758480327a8cf3069b6f4c3dd439652b75a
javaht <java_ht@163.com>
2022-04-04 20:50:26 +0800

    nothing

--- src/main/java/com/zht/partitioner/MyPartitioner.java
+++ src/main/java/com/zht/partitioner/MyPartitioner.java
@@ -5,15 +5,25 @@ import org.apache.kafka.common.Cluster;
 
 import java.util.Map;
 
+//生产者的自定义分区
 public class MyPartitioner  implements Partitioner {
 
     @Override
     public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
 
-        Integer integer = cluster.partitionCountForTopic(topic);
+        String msgValue = value.toString();
+
+        int partition;
+
+        if(msgValue.contains("atguigu")){
+            partition =0;
+        }else{
+            partition=1;
+        }
+        //Integer integer = cluster.partitionCountForTopic(topic);
 
 
-        return 2;
+        return partition;
     }
 
     @Override
